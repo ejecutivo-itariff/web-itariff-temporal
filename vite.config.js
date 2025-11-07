4c59d8cad1591a39c9a2e2bdc6d8d5b66a998439
Mauricio <ejecutivo.tecnologia@itariff.com.mx>
2025-11-07 16:57:29 -0600

    json updates 2

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/web-itariff-temporal/',
   plugins: [
     // The official Tailwind CSS plugin for Vite (v4.0+)
     tailwindcss(),
